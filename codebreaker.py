b59b353bd280857a007b23a390be108e5da8728e
DrManishTaram <68514597+DrManishTaram@users.noreply.github.com>
2025-08-26 09:17:53 +0530

    Update codebreaker.py

--- codebreaker.py
+++ codebreaker.py
@@ -63,6 +63,4 @@ def decrypt(req: DecryptRequest):
         quantum_result=quantum_result
     )
 
-# --- Run Uvicorn if executed directly ---
-if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+
